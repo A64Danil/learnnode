0a384e20cd84871d47e1d7400e7a950ce11afd54
DanilZen <a64danil@mail.ru>
2019-05-28 19:05:52 +0300

    Express setup done

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+var express =  require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+    console.log("something happens");
+    res.send('<h1>Hello world</h1>');
+})
+
+app.listen(3000);
+console.log('Server is running on port 3000...')
