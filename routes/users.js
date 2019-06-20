e3f41b6062cd803956a53cfb0666485d1f5de8c6
DanilZen <a64danil@mail.ru>
2019-06-20 09:36:47 +0300

    add form validatin to refister.jade and users.js, add uploads to ignore

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './upload'});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,5 +16,48 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
+
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  console.log(req.file);
+
+  if (req.file) {
+    console.log('Uploading File...')
+    var profileimage = req.file.filename;
+  } else {
+    console.log('No FIle Uploaded...')
+    var profileimage = 'noimage.jpg';
+  }
+
+  // Form Validator
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  if(errors) {
+    console.log('Errors');
+    res.render('register', {
+      errors: errors
+    })
+
+  } else {
+    console.log('No Errors');
+  }
+
+});
+
 
 module.exports = router;
