99462aa63db0266b1680df56974b409b1f96eec6
DanilZen <a64danil@mail.ru>
2019-07-16 22:11:37 +0300

    add cart (part 2 of 2)

--- TekBooks/controllers/cart.js
+++ TekBooks/controllers/cart.js
@@ -9,7 +9,7 @@ module.exports = function (router) {
         // get cart from session
         var cart = req.session.cart;
         var displayCart = {
-            item: [],
+            items: [],
             total: 0
         };
         var total = 0;
@@ -27,4 +27,29 @@ module.exports = function (router) {
             cart: displayCart
         });
     });
+
+
+    router.post('/:id', function (req, res) {
+        req.session.cart = req.session.cart || {};
+        var cart = req.session.cart;
+        
+        Book.findOne({_id: req.params.id}, function (err, book) {
+            if (err) {
+                console.log(err)
+            }
+
+            if(cart[req.params.id]) {
+                cart[req.params.id].qty++
+            } else {
+                cart[req.params.id] = {
+                    item: book._id,
+                    title: book.title,
+                    price: book.price,
+                    qty: 1
+                }
+            }
+
+            res.redirect('/cart')
+        })
+    })
 }
