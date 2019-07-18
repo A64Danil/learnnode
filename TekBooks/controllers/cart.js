fc85fc0c26274b35b7bc66b6b9bc20a53f1a45c4
DanilZen <a64danil@mail.ru>
2019-07-18 18:22:28 +0300

    add buttons to change quanity of items in cart and alerts about it

--- TekBooks/controllers/cart.js
+++ TekBooks/controllers/cart.js
@@ -5,6 +5,7 @@ var Category = require('../models/categoryModel');
 
 module.exports = function (router) {
 
+    // Get cart
     router.get('/', function (req, res) {
         // get cart from session
         var cart = req.session.cart;
@@ -29,6 +30,7 @@ module.exports = function (router) {
     });
 
 
+    // Add item to cart
     router.post('/:id', function (req, res) {
         req.session.cart = req.session.cart || {};
         var cart = req.session.cart;
@@ -53,6 +55,7 @@ module.exports = function (router) {
         })
     });
 
+    // Empty cart
     router.get('/remove', function (req, res) {
         req.session.cart = {};
         res.location('/cart?type=success&text=Cart is emty now!');
@@ -60,6 +63,7 @@ module.exports = function (router) {
     });
 
 
+    // Remove one item from cart
     router.post('/remove/:id', function (req, res) {
         var cart = req.session.cart;
 
@@ -70,6 +74,61 @@ module.exports = function (router) {
 
         res.location('/cart?type=success&text=Book "' + bookName + '" was deleted!');
         res.redirect('/cart?type=success&text=Book "' + bookName + '" was deleted!');
+    });
+
+    // Change qnty of selected item to increment (add one more)
+    router.post('/addone/:id', function (req, res) {
+        req.session.cart = req.session.cart || {};
+        var cart = req.session.cart;
+        var bookName = cart[req.params.id].title;
+
+        Book.findOne({_id: req.params.id}, function (err, book) {
+            if (err) {
+                console.log(err)
+            }
+
+            if(cart[req.params.id]) {
+                cart[req.params.id].qty++
+            } else {
+                res.location('/cart?type=error&text=You do not have book named "' + bookName + '" in you cart. So you can not add one more');
+                res.redirect('/cart?type=error&text=You do not have book named "' + bookName + '" in you cart. So you can not add one more');
+            }
+
+            res.location('/cart?type=success&text=Add one more copy of "' + bookName + '"');
+            res.redirect('/cart?type=success&text=Add one more copy of "' + bookName + '"');
+        })
+    });
+
+    // Change qnty of selected item to decrement (remove one left)
+    router.post('/removeone/:id', function (req, res) {
+        req.session.cart = req.session.cart || {};
+        var cart = req.session.cart;
+        var bookName = cart[req.params.id].title;
+
+        Book.findOne({_id: req.params.id}, function (err, book) {
+            if (err) {
+                console.log(err)
+            }
+
+            if(cart[req.params.id]) {
+                if(cart[req.params.id].qty > 1) {
+                    cart[req.params.id].qty--;
+                    res.location('/cart?type=success&text=Remove one copy of "' + bookName + '"');
+                    res.redirect('/cart?type=success&text=Remove one copy of "' + bookName + '"');
+                } else if(cart[req.params.id].qty === 1) {
+                    res.location('/cart?type=error&text=You can not remove, because you have only one item. Use delete button instead.');
+                    res.redirect('/cart?type=error&text=You can not remove, because you have only one item. Use delete button instead.');
+                } else {
+                    delete cart[req.params.id];
+                    req.session.cart = cart;
+                }
+
+
+            } else {
+                res.location('/cart?type=error&text=You do not have book named "' + bookName + '" in you cart. So you can not remove it');
+                res.redirect('/cart?type=error&text=You do not have book named "' + bookName + '" in you cart. So you can not remove it');
+            }
+        })
     });
 
 };
