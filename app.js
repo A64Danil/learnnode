2f9be0b21c4c24c176e7b96fb3f41b5ef0667827
DanilZen <a64danil@mail.ru>
2019-07-24 13:28:12 +0300

    add new routes, change engine from Jade to Handlebars, remove expressValidator for a while error is exist

--- app.js
+++ app.js
@@ -1,65 +1,85 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var session = require('express-session');
 var flash = require('connect-flash');
 var multer = require('multer');
-var upload = multer({ dest: './public/images/portfolio' });
+var upload = multer({dest: './public/imgages/portfolio'});
 
-// Route Files
-var routes = require('./routes/index');
-var admin = require('./routes/admin');
 
-// Init App
+var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
+
 var app = express();
 
-// Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// Handle Sessions
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session({
-  secret:'secret',
+  secret: 'secret',
   saveUninitialized: true,
   resave: true
 }));
 
 // Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+// app.use(expressValidator({
+//   errorFormatter: function(param, msg, value) {
+//     var namespace = param.split('.')
+//         , root    = namespace.shift()
+//         , formParam = root;
+//
+//     while(namespace.length) {
+//       formParam += '[' + namespace.shift() + ']';
+//     }
+//     return {
+//       param : formParam,
+//       msg   : msg,
+//       value : value
+//     };
+//   }
+// }));
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
 
-// Public Folder
-app.use(express.static(path.join(__dirname, 'public')));
 
-// View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// Connect Flash
 app.use(flash());
 
-app.use('/', routes);
-app.use('/admin', admin);
+app.use('/', indexRouter);
+app.use('/admin', adminRouter);
 
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port: '+app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('Server started on port: ' +app.get('port'))
 });
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
