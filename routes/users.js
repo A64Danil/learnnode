981d5d7c03b7cf03b0ddff61a0a1cc28657c819e
DanilZen <a64danil@mail.ru>
2019-08-05 15:37:05 +0300

    add form validation to users/register

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var validation = require('validator');
 
 // Unclude User Model
 var User = require('../models/user');
@@ -45,13 +46,18 @@ router.post('/register', function (req, res, next) {
   // req.checkBody('password', 'Password field is required').notEmpty();
   // req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
+  var errors = [];
+  if(validation.isEmpty(req.body.first_name)) errors.push({msg: "First name is empty. First name is requires."});
+  if(validation.isEmpty(req.body.last_name)) errors.push({msg: "Last name is empty. Last name is requires."});
+  if(validation.isEmpty(req.body.email)) errors.push({msg: "Email is empty. Email is requires."});
+  if(validation.isEmail(req.body.email)) errors.push({msg: "Email must be a valid email address."});
+  if(validation.isEmpty(req.body.username)) errors.push({msg: "Username is empty. Username is requires."});
+  if(validation.isEmpty(req.body.password)) errors.push({msg: "Password is empty. Password is requires."});
+  if(validation.isEmpty(req.body.password2)) errors.push({msg: "Password2 is empty. Password2 is requires."});
+  if(!validation.equals(req.body.password2, req.body.password)) errors.push({msg: "Passwords do not match."});
+
   console.log('router.post AFTER validation');
 
-  errors = [{
-    msg: 'testing some'
-  },{
-    msg: 'testing some two'
-  }];
 
   if (errors) {
     console.log('Errors');
