55f4c98d572a2b7a913b7c1a0ccba178e16ff3f9
DanilZen <a64danil@mail.ru>
2020-01-10 19:30:20 +0300

    start express server, add dust engine and base settings

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg'),
+    app = express();
+
+// DB Conntect String
+var connect = "postres://onix:admin@localhost/recipeboookdb";
+
+// Assign Dust Engine To .dust Files
+app.engine('dust', cons.dust);
+
+// Set Default Ext .dust
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// Server
+app.listen(3000, function () {
+    console.log('Server Started on Port 3000');
+})
